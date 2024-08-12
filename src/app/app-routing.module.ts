3e2b296a778e0da070cd791d3a8b0fd2d180f52e
NikitaGawrilov <gawr.nik@gmail.com>
2024-08-12 23:03:02 +0500

    basic setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './pages/main-page/main-page.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
